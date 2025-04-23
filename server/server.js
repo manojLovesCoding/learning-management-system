e787e51cf13589a667d96430a83a3baa79120a69
manojLovesCoding <mkum9417@gmail.com>
2025-04-23 16:46:33 +0530

    create APIs for course

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { clerkWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
 import connectCloudinary from "./configs/cloudinary.js";
+import courseRouter from "./routes/courseRoutes.js";
 
 //initialize express
 const app = express();
@@ -22,6 +23,7 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("API WORKING"));
 app.post("/clerk", express.json(), clerkWebhooks);
 app.use("/api/educator", express.json(), educatorRouter);
+app.use("/api/course", express.json(), courseRouter);
 
 //port
 const PORT = process.env.PORT || 5000;
